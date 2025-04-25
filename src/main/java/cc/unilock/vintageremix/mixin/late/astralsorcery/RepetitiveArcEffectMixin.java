1a1600d1008ba8ad47efa06e0159fb2b4c5397a9
unilock <unilock@fennet.rentals>
2025-04-25 18:44:16 -0400

    fix RepetitiveArcEffectMixin
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/vintageremix/mixin/late/astralsorcery/RepetitiveArcEffectMixin.java
+++ src/main/java/cc/unilock/vintageremix/mixin/late/astralsorcery/RepetitiveArcEffectMixin.java
@@ -1,11 +1,13 @@
 package cc.unilock.vintageremix.mixin.late.astralsorcery;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Coerce;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -16,8 +18,13 @@ public class RepetitiveArcEffectMixin {
 	@Shadow
 	private EntityPlayer player;
 
-	@Inject(method = "fire", at = @At(value = "INVOKE_ASSIGN", target = "Ljava/util/List;removeAll(Ljava/util/Collection;)Z"))
-	private void fire$removeAll(CallbackInfo ci, @Local(ordinal = 1) List<EntityLivingBase> entities) {
+	@WrapWithCondition(method = "fire", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"))
+	private boolean fire$modifyVisitedEntities(List<EntityLivingBase> instance, @Coerce Object entity) {
+		return !((EntityLivingBase) entity).isOnSameTeam(this.player);
+	}
+
+	@Inject(method = "fire", at = @At(value = "INVOKE", target = "Ljava/util/List;removeAll(Ljava/util/Collection;)Z"))
+	private void fire$modifyEntities(CallbackInfo ci, @Local(ordinal = 1) List<EntityLivingBase> entities) {
 		entities.removeIf(e -> e.isOnSameTeam(this.player));
 	}
 }
