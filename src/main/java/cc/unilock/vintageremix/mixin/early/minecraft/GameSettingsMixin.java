39e66dcf665700e47bf4f6e35652c526c5c12dd4
unilock <unilock@fennet.rentals>
2025-06-06 15:47:46 -0400

    catch GameSettings#getSoundLevel exception
    seems to be due to Wizardry
    
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/cc/unilock/vintageremix/mixin/early/minecraft/GameSettingsMixin.java
@@ -0,0 +1,20 @@
+package cc.unilock.vintageremix.mixin.early.minecraft;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.client.settings.GameSettings;
+import net.minecraft.util.SoundCategory;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(GameSettings.class)
+public class GameSettingsMixin {
+	@WrapMethod(method = "getSoundLevel")
+	private float getSoundLevel(SoundCategory category, Operation<Float> original) {
+		try {
+			return original.call(category);
+		} catch (Throwable e) {
+//			VintageRemix.LOGGER.debug("GameSettings#getSoundLevel for SoundCategory "+category.getName()+" failed!");
+			return 1.0F;
+		}
+	}
+}
