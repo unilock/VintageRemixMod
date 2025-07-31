299e73b10e6fe45a940980e65546c9062f9c3cc9
unilock <unilock@fennet.rentals>
2025-07-31 11:23:29 -0400

    attempt to silence geckolib errors

--- src/main/java/cc/unilock/vintageremix/mixin/late/geckolib3/GeckoLibCacheMixin.java
+++ src/main/java/cc/unilock/vintageremix/mixin/late/geckolib3/GeckoLibCacheMixin.java
@@ -10,12 +10,12 @@ import software.bernie.geckolib3.resource.GeckoLibCache;
 public abstract class GeckoLibCacheMixin {
 	// Silence extraneous logging
 
-	@Redirect(method = "onResourceManagerReload", at = @At(value = "INVOKE", target = "Ljava/lang/Exception;printStackTrace()V"))
+	@Redirect(method = "onResourceManagerReload", at = @At(value = "INVOKE", target = "Ljava/lang/Exception;printStackTrace()V"), remap = true)
 	private void onResourceManagerReload$printStackTrace(Exception instance) {
 		// NO-OP
 	}
 
-	@Redirect(method = "onResourceManagerReload", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;error(Ljava/lang/String;Ljava/lang/Throwable;)V"))
+	@Redirect(method = "onResourceManagerReload", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;error(Ljava/lang/String;Ljava/lang/Throwable;)V"), remap = true)
 	private void onResourceManagerReload$error(Logger instance, String s, Throwable throwable) {
 		instance.error(s);
 	}
