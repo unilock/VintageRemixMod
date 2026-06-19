d5bb1b125d91b6e03e034df471b7017e66615732
unilock <katherines1337@outlook.com>
2026-06-19 04:54:56 -0400

    don't rely on mixinextras expressions i guess

--- src/main/java/cc/unilock/vintageremix/mixin/late/botania/ItemFlightTiaraMixin.java
+++ src/main/java/cc/unilock/vintageremix/mixin/late/botania/ItemFlightTiaraMixin.java
@@ -1,11 +1,10 @@
 package cc.unilock.vintageremix.mixin.late.botania;
 
-import com.llamalad7.mixinextras.expression.Definition;
-import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import vazkii.botania.common.item.ItemTemperanceStone;
@@ -13,10 +12,8 @@ import vazkii.botania.common.item.equipment.bauble.ItemFlightTiara;
 
 @Mixin(value = ItemFlightTiara.class, remap = false)
 public class ItemFlightTiaraMixin {
-	@Definition(id = "cooldown", local = @Local(name = "cooldown"))
-	@Expression("cooldown == 0")
-	@WrapOperation(method = "onWornTick", at = @At("MIXINEXTRAS:EXPRESSION"))
-	private boolean onWornTick$cooldown(int left, int right, Operation<Boolean> original, @Local(name = "p") EntityPlayer p) {
-		return original.call(left, right) && !ItemTemperanceStone.hasTemperanceActive(p);
+	@WrapOperation(method = "onWornTick", at = @At(value = "INVOKE", target = "Lvazkii/botania/common/core/helper/ItemNBTHelper;getInt(Lnet/minecraft/item/ItemStack;Ljava/lang/String;I)I", ordinal = 1))
+	private int onWornTick$getInt(ItemStack stack, String tag, int defaultExpected, Operation<Integer> original, @Local(name = "p") EntityPlayer p) {
+		return ItemTemperanceStone.hasTemperanceActive(p) ? -1 : original.call(stack, tag, defaultExpected);
 	}
 }
